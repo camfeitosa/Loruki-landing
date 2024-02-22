73f1314a6041b802623f5fda770b00e342c6a90f
Camila Feitosa <114369214+camfeitosa@users.noreply.github.com>
2024-02-22 11:26:56 -0300

    bd

--- conexao.php
+++ conexao.php
@@ -1,7 +1,7 @@
 <?php
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:camilaf.database.windows.net,1433; Database = loja", "camDB", "{your_password_here}");
+    $conn = new PDO("sqlsrv:server = tcp:camilaf.database.windows.net,1433; Database = loja", "camDB", "Camila050406");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
@@ -10,7 +10,7 @@ catch (PDOException $e) {
 }
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "camDB", "pwd" => "{your_password_here}", "Database" => "loja", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "camDB", "pwd" => "Camila050406", "Database" => "loja", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:camilaf.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 
