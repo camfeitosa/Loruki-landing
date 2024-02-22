27d5a06cf52c1f265afc55356541f8c248fc4505
Camila Feitosa <114369214+camfeitosa@users.noreply.github.com>
2024-02-22 11:18:52 -0300

    bd

---
+++ conexao.php
@@ -0,0 +1,32 @@
+<?php
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:camilaf.database.windows.net,1433; Database = loja", "camDB", "{your_password_here}");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "camDB", "pwd" => "{your_password_here}", "Database" => "loja", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:camilaf.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+// Recupera os dados do formulário
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['senha'];
+
+// Insere os dados no banco de dados
+$sql = "INSERT INTO contato (nome, email, senha) VALUES ('$name', '$email', '$senha')";
+if ($conn->query($sql) === TRUE) {
+    echo "Mensagem enviada com sucesso!";
+} else {
+    echo "Erro: " . $sql . "<br>" . $conn->error;
+}
+
+// Fecha a conexão com o banco de dados
+$conn->close();
+?>
